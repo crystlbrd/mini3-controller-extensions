4d7cf2dfd4d9dfcd2be29c3e884359bc9b1a046e
Emanuel Bitkov <crystlbrd@bitkov.de>
2020-04-17 14:51:16 +0200

    Added AllExtensions A class to quickly use all available Traits

---
+++ src/AllExtensions.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace crystlbrd\ControllerExt;
+
+
+use crystlbrd\ControllerExt\ControllerTraits\Router;
+
+/**
+ * Includes all Controller Traits
+ * @package crystlbrd\ControllerExt
+ */
+class AllExtensions
+{
+    use Router;
+}
